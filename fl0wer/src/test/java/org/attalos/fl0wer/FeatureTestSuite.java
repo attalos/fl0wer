e3fd30cdc72c54236686100ad8f30700da8070f3
Attalos <friedrichmichel95@gmail.com>
2018-02-16 14:48:51 +0100

    fixed import error in TestSuite
    Former-commit-id: 916c1ba8c90fafc2c39bbc63c529f744e357b8c3

--- fl0wer/src/test/java/org/attalos/fl0wer/FeatureTestSuite.java
+++ fl0wer/src/test/java/org/attalos/fl0wer/FeatureTestSuite.java
@@ -1,14 +1,9 @@
 package org.attalos.fl0wer;
 
 ;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.attalos.fl0wer.controll.ConstantValues;
-import org.attalos.fl0wer.controll.FL_0_subsumption;
+
 import org.attalos.fl0wer.controll.FL_0_subsumptionTest;
 import org.attalos.fl0wer.rete.ReteNetworkTest;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
