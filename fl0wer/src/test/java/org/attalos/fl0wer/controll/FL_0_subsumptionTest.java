5fb3d6b194b9e729135e0a7a20b8dc7db3277998
Attalos <friedrichmichel95@gmail.com>
2018-02-16 10:51:37 +0100

    added first blocking condition test
    Former-commit-id: 442446da64bf8a647612fca2ac2bf4858660ec01

--- fl0wer/src/test/java/org/attalos/fl0wer/controll/FL_0_subsumptionTest.java
+++ fl0wer/src/test/java/org/attalos/fl0wer/controll/FL_0_subsumptionTest.java
@@ -14,6 +14,7 @@ import org.semanticweb.owlapi.model.*;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +22,9 @@ public class FL_0_subsumptionTest {
     private FL_0_subsumption fl_0_subsumption;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private OWLClass root_concept_owl;
+
+    OWLOntologyManager manager;
+    OWLDataFactory factory;
 
     @BeforeClass
     public static void runOnceBeforeClass() {
@@ -35,35 +39,63 @@ public class FL_0_subsumptionTest {
     @Before
     public void setUp() throws Exception {
         /* owl init */
-        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        OWLDataFactory factory = manager.getOWLDataFactory();
-        OWLOntologyManager m = OWLManager.createOWLOntologyManager();
+        manager = OWLManager.createOWLOntologyManager();
+        factory = manager.getOWLDataFactory();
 
         /* open ontology */
         File ontology_file = new File("src/main/resources/file/test-ontology-2.owl");
-        OWLOntology ontology_owl = ontology_owl = m.loadOntologyFromOntologyDocument(ontology_file);
+        OWLOntology ontology_owl = ontology_owl = manager.loadOntologyFromOntologyDocument(ontology_file);
 
         /* get owl class of input classes */
         root_concept_owl = factory.getOWLClass(IRI.create("http://www.semanticweb.org/attalos/ontologies/2017/6/testing-ontology#B"));
         Concept_Factory.getInstance().get_concept_from_owl_class(root_concept_owl);     //make sure it has a number representation
 
         fl_0_subsumption = new FL_0_subsumption(ontology_owl);
-
-        //read output stream
-        System.setOut(new PrintStream(outContent));
     }
 
     @Test
     public void decide_subsumption() {
+        //read output stream
+        PrintStream stdout = System.out;
+        System.setOut(new PrintStream(outContent));
+
         fl_0_subsumption.calculate_subsumerset(root_concept_owl);
         String given_answer = outContent.toString().replace("\n", "");
         String corret_answer = "<http://www.semanticweb.org/attalos/ontologies/2017/6/testing-ontology#E><http://www.semanticweb.org/attalos/ontologies/2017/6/testing-ontology#B>";
 
         assertEquals("Answer to subsumptiontask was wrong", corret_answer, given_answer);
+
+        //reset output stream to stdout
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void testBlocking() {
+        fl_0_subsumption = new FL_0_subsumption(openOwlOntology("src/test/resources/blockingTestOntology.owl"));
+
+        /* get owl class of input classes */
+        root_concept_owl = factory.getOWLClass(IRI.create("http://www.semanticweb.org/attalos/ontologies/2018/1/blockingTestOntology#A"));
+
+        //if this runs for to long - blocking probably doesn't work
+        List<OWLClass> subsumerset = fl_0_subsumption.calculate_subsumerset(root_concept_owl);
+
+        assertNotNull(subsumerset);
+
     }
 
     @AfterClass
     public static void run_once_after_Class() {
         ConstantValues.purge();
+    }
+
+    public OWLOntology openOwlOntology(String ontology_path) {
+        /* open ontology */
+        File ontology_file = new File(ontology_path);
+        try {
+            return manager.loadOntologyFromOntologyDocument(ontology_file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
