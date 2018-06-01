a2e41d38dcaacf446d215b3078c5cc08eb1f888a
Attalos <friedrichmichel95@gmail.com>
2018-06-01 14:38:03 +0200

    added error bars to plot
    Former-commit-id: 55e09e1b30e24852e13d05546bcd34ef75157d02

--- executionScript/plotEval.R
+++ executionScript/plotEval.R
@@ -2,6 +2,27 @@
 require(data.table)
 X11()
 
+mean_f <- function(data) {
+	meanValue = mean(data)
+	if (meanValue < 1) {
+		meanValue = 1
+	}
+	return(meanValue)
+}
+
+error_f <- function(data) {
+	error = sd(data)/sqrt(length(data))
+	return(error)
+}
+
+reasonerData_f <- function(data, reasonerName) {
+	return(dt[reasoner==reasonerName, .(mean=mean_f(time), error=error_f(time)), .(ontology, classcount)][order(classcount)][,.(classcount,mean,error)])
+}
+
+errorbar_f <- function(data, col) {
+	arrows(c(data[,classcount]), data[,mean] - data[,error], c(data[,classcount]), data[,mean] + data[,error], length=0.05, angle=90, code=3, col=col)
+}
+
 args = commandArgs(trailingOnly = TRUE)
 dt = fread(args[1], sep=",", header=TRUE)
 
@@ -20,29 +41,41 @@ if (length(args) == 2) {
 #ggplot(dt[reasoner=="Fl0wer", mean(time), .(ontology, classcount)][order(classcount)][,.(classcount,V1)], aes(x=classcount, y=V1)) + geom_point() + geom_line()
 #ylim=range(t[2],tt[2])
 
-flowerData = dt[reasoner=="Fl0wer", if (mean(time) == 0) 1 else mean(time), .(ontology, classcount)][order(classcount)][,.(classcount,V1)]
-hermitData = dt[reasoner=="Hermit", if (mean(time) == 0) 1 else mean(time), .(ontology, classcount)][order(classcount)][,.(classcount,V1)]
-openlletData = dt[reasoner=="Openllet", if (mean(time) == 0) 1 else mean(time), .(ontology, classcount)][order(classcount)][,.(classcount,V1)]
-
+flowerData = reasonerData_f(dt, "Fl0wer")
+hermitData = reasonerData_f(dt, "Hermit")
+openlletData = reasonerData_f(dt, "Openllet")
 
 if (ignore_jfact) {
-	plot(flowerData, type="b", col="blue", ylim=range(flowerData[,2],hermitData[,2],openlletData[,2]), log=log_graph, ylab="time [ms]")
+	ylim = range(flowerData[,mean] + flowerData[,error], hermitData[,mean] + hermitData[,error], openlletData[,mean] + openlletData[,error])
+	plot(flowerData[,classcount], flowerData[,mean], type="b", pch="-", col="blue", ylim=ylim, log=log_graph, ylab="time [ms]", xlab="classcount")
 	
-	lines(hermitData, type="b", col="red")
-	lines(openlletData, type="b", col="green")
+	errorbar_f(flowerData, "blue")
+	errorbar_f(hermitData, "red")
+	errorbar_f(openlletData, "green")
+	
+	lines(hermitData, type="b", pch="-", col="red")
+	lines(openlletData, type="b", pch="-", col="green")
 	
 	legend("topleft", inset=.04, legend=c("Fl0wer", "Hermit", "Openllet"), col=c("blue", "red", "green"), lty=1)
 } else {
-	jfactData = dt[reasoner=="JFact", if (mean(time) == 0) 1 else mean(time), .(ontology, classcount)][order(classcount)][,.(classcount,V1)]
+	jfactData = reasonerData_f(dt, "JFact")
 	
-	plot(flowerData, type="b", col="blue", ylim=range(flowerData[,2],hermitData[,2],openlletData[,2],jfactData[,2]), log=log_graph, ylab="time [ms]")
+	ylim = range(flowerData[,mean] + flowerData[,error], hermitData[,mean] + hermitData[,error], openlletData[,mean] + openlletData[,error], jfactData[,mean] + jfactData[,error])
+	plot(flowerData[,classcount], flowerData[,mean], type="b", pch="-", col="blue", ylim=ylim, log=log_graph, ylab="time [ms]", xlab="classcount")
 	
-	lines(hermitData, type="b", col="red")
-	lines(openlletData, type="b", col="green")
-	lines(jfactData, type="b", col="black")
+	errorbar_f(flowerData, "blue")
+	errorbar_f(hermitData, "red")
+	errorbar_f(openlletData, "green")
+	errorbar_f(jfactData, "black")
+	
+	lines(hermitData, type="b", pch="-", col="red")
+	lines(openlletData, type="b", pch="-", col="green")
+	lines(jfactData, type="b", pch="-", col="black")
 	
 	legend("topleft", inset=.04, legend=c("Fl0wer", "Hermit", "Openllet", "JFact"), col=c("blue", "red", "green", "black"), lty=1)
 }
+
+#arrows(bla, , xx, up, col = repcols, angle = 90, length = 0.03, code = 3)
 
 message("Press return to continue")
 invisible(readLines("stdin", n=1))
