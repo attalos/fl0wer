c372d784436158f5dc9dd9f115d55600fc403a94
Friedrich Michel <friedrich.michel@tu-dresden.de>
2019-03-01 16:40:17 +0100

    working on plotting
    Former-commit-id: 7bd1d5e10d30dbd4067e1353d441b503c734df6c

--- eval/plot.py
+++ eval/plot.py
@@ -3,14 +3,14 @@
 import pandas
 import sys
 import numpy as np
+import argparse
+import warnings
+import subprocess
 import rpy2.robjects.packages as rpackages
 import rpy2.robjects as robj
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.lib import grid
 from rpy2.robjects.packages import importr
-
-utils = rpackages.importr('utils')
-utils.chooseCRANmirror(ind=1) # select the first mirror in the list
 
 # ggplot2
 def install_packages():
@@ -24,76 +24,77 @@ def install_packages():
     # We are fancy, just because we can.
     names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
     if len(names_to_install) > 0:
+        utils = rpackages.importr('utils')
+        utils.chooseCRANmirror(ind=1) # select the first mirror
         utils.install_packages(StrVector(names_to_install))
 
+# import ggplot2
 install_packages()
+warnings.filterwarnings("ignore", category=UserWarning)
 import rpy2.robjects.lib.ggplot2 as ggplot2
 
 
 def mean_of_valid(values):
     return np.mean([x for x in values if x != -1])
 
-#params
-if len(sys.argv) != 2:
-    print("Output file needed")
-    exit(1)
-
-filename = sys.argv[1]
-
-#device
-grdevices = importr('grDevices')
-grdevices.pdf(file="graph.pdf", width=20, height=10)
+# r functions
+r_TRUE = robj.r("TRUE")
+r_FALSE = robj.r("FALSE")
 
 ########
 # MAIN #
 ########
-df = pandas.read_csv(filename)
-df["mean_time"] = df["time"].groupby(df["ontology"]).transform(mean_of_valid)
-
-robj.pandas2ri.activate()
-plotting_data_R = robj.conversion.py2ri(df)
+# argument parsing
+parser = argparse.ArgumentParser(description="Plot the given files")
+parser.add_argument("filenames", metavar="<filename>", type=str, nargs="+", help="csv input file (output of execute.sh)")
+parser.add_argument("-o", "--output", metavar="<filename>", type=str, default="graph.pdf", help="the graph will be written to this file")
+parser.add_argument("--open", action="store_true", help="open the graph with the default application (xdg-open <filename>)")
+args = parser.parse_args()
 
 # page
+grdevices = importr('grDevices')
+grdevices.pdf(file=args.output, width=20, height=10*len(args.filenames))
 grid.newpage()
-lt = grid.layout(1, 2)
+lt = grid.layout(len(args.filenames), 2)
 vp = grid.viewport(layout = lt)
 vp.push()
 
-# diagram one - dependent on classcount
-gp = ggplot2.ggplot(plotting_data_R)
-pp = gp + \
-     ggplot2.aes_string(x="classcount", y="time", colour="reasoner", shape="reasoner") + \
-     ggplot2.geom_point() + \
-     ggplot2.scale_x_log10() + \
-     ggplot2.scale_y_log10()
-vp = grid.viewport(**{'layout.pos.col':1, 'layout.pos.row': 1})
-pp.plot(vp=vp)
+row = 0
+for filename in args.filenames:
+    row += 1
 
-# diagram two - dependent on mean time
-pp = gp + \
-     ggplot2.aes_string(x="mean_time", y="time", colour="reasoner", shape="reasoner") + \
-     ggplot2.geom_point() + \
-     ggplot2.scale_x_log10() + \
-     ggplot2.scale_y_log10()
-vp = grid.viewport(**{'layout.pos.col':2, 'layout.pos.row': 1})
-pp.plot(vp=vp)
+    df = pandas.read_csv(filename)
+    df["mean_time"] = df["time"].groupby(df["ontology"]).transform(mean_of_valid)
 
-input("Press Enter to continue...")
+    robj.pandas2ri.activate()
+    plotting_data_R = robj.conversion.py2ri(df)
 
 
+    # diagram one - dependent on classcount
+    # ggplot2.geom_smooth(method="lm", formula="y ~ splines::bs(x, 3)", se=r_FALSE) + \
+    # https://en.wikipedia.org/wiki/Local_regression
+    gp = ggplot2.ggplot(plotting_data_R)
+    pp = gp + \
+        ggplot2.aes_string(x="classcount", y="time", colour="reasoner", shape="reasoner") + \
+        ggplot2.geom_point() + \
+        ggplot2.geom_smooth(method="loess", se=r_FALSE) + \
+        ggplot2.scale_x_log10() + \
+        ggplot2.scale_y_log10()
+    vp = grid.viewport(**{'layout.pos.col':1, 'layout.pos.row': row})
+    pp.plot(vp=vp)
 
+    # diagram two - dependent on mean time
+    pp = gp + \
+        ggplot2.aes_string(x="mean_time", y="time", colour="reasoner", shape="reasoner") + \
+        ggplot2.geom_point() + \
+        ggplot2.geom_smooth(method="loess", se=r_FALSE) + \
+        ggplot2.scale_x_log10() + \
+        ggplot2.scale_y_log10()
+    vp = grid.viewport(**{'layout.pos.col':2, 'layout.pos.row': row})
+    pp.plot(vp=vp)
 
-##df.set_index(["taskID", "reasonr", "ontology"], inplace=True)
-##
-##print(df)
-##
-### transform the data in an easyer to print table
-##plotting_data = pandas.DataFrame(columns=["classcount", "ontology"])
-##for reasoner in ["Fl0wer", "Hermit", "Openllet", "JFact"]:
-##    tmp_df = df.loc[0].loc[reasoner, ["classcount", "time"]]
-##    tmp_df.rename(columns={"time":reasoner}, inplace=True)
-##    plotting_data = pandas.merge(plotting_data, tmp_df, on=["ontology", "classcount"], how="outer")
-##
-##print(plotting_data)
-##
-##plotting_data_R = robj.conversion.py2ri(plotting_data)
+#grdevices.dev.off()
+robj.r("dev.off()")
+
+if args.open:
+    subprocess.call(("xdg-open", args.output))
